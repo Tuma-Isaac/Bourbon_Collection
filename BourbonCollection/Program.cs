ebf483938a1f7d06308df5d5b11dcde137d314b5
isaactuma <tuma.isaac@gmail.com>
2021-07-07 20:51:46 -0400

    Created a list of options for the user using stringbuilder. Implemented a master while loop using the list of options. Added functionality to the loop by creating a break point if the user types 'quit'. Prints the list of bottles to console if user selects 1.

--- BourbonCollection/Program.cs
+++ BourbonCollection/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.IO;
 using CsvHelper;
 using System.Globalization;
+using System.Text;
 
 namespace BourbonCollection
 {
@@ -18,9 +19,74 @@ namespace BourbonCollection
             var fileName = Path.Combine(directory.FullName, "BourbonCollection.csv");
             _bourbonBottles = ReadBourbonCollection(fileName);
 
+            // List of options 
 
-            PrintBottles(_bourbonBottles);
+            StringBuilder options = new StringBuilder();
+
+            options.Append("\n");
+            options.Append("\nWelcome to the Bourbon Collection");
+            options.Append("\n****************************");
+            options.Append("\nTo view the current collection, press 1.");
+            options.Append("\nTo add a new bottle, press 2");
+            options.Append("\nTo update a current bottle, press 3");
+            options.Append("\nTo remove a bottle, press 4");
+            options.Append("\nTo calculate the value of the collection, press 5");
+            options.Append("\n****************************");
+            options.Append("\nType quit to quit");
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine(options);
+                    var response = Console.ReadLine();
+                  
+
+                    if (response.ToLower() == "quit")
+                    {
+                        break;
+                    }
+
+                    // View current collection
+                    else if (Int32.Parse(response) == 1)
+                    {
+                        _bourbonBottles = ReadBourbonCollection(fileName);
+                        Console.WriteLine();
+                        PrintBottles(_bourbonBottles);
+                        continue;
+
+                    }
+                    // Add a new bottle to the collection
+                    else if (Int32.Parse(response) == 2)
+                    {
+
+
+
+                    }
+                    // Update an existing bottle count
+                    else if (Int32.Parse(response) == 3)
+                    {
+                       
+                    }
+                    // Remove a bottle from collection
+                    else if (Int32.Parse(response) == 4)
+                    {
+                       
+                    }
+                    // Calculate value of collection
+                    else if (Int32.Parse(response) == 5)
+                    {
+                      
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Please enter a valid input.");
+                }
+            }
         }
+
+
 
 
         // Read data from file
@@ -36,6 +102,8 @@ namespace BourbonCollection
 
             return bourbonCollection;
         }
+
+     
 
         //Print bottles to console 
         static void PrintBottles(List<Bottle> bottles)
