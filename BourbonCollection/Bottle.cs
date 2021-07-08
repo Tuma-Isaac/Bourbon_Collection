6347cd50821264e30aa156ff8fb8674aaa1641ba
isaactuma <tuma.isaac@gmail.com>
2021-07-07 20:21:58 -0400

    Added a csv file, created a function to read the data from csv file and store said data in the bottle class. Created a method to print the datato the console.

---
+++ BourbonCollection/Bottle.cs
@@ -0,0 +1,38 @@
+﻿using CsvHelper.Configuration.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BourbonCollection
+{
+    class Bottle
+    {
+        [Name("Name")]
+        public string Name { get; set; }
+        [Name("Distillery")]
+        public string Distillery { get; set; }
+        [Name("Size(ml/L)")]
+        public double Size { get; set; }
+        [Name("Proof")]
+        public double Proof { get; set; }
+        [Name("Age")]
+        public string Age { get; set; }
+        [Name("MSRP")]
+        public int MSRP { get; set; }
+        [Name("SecondaryPrice")]
+        public int SecondaryPrice { get; set; }
+        [Name("BottlesOwned")]
+        public int BottlesOwned { get; set; }
+
+
+
+        public override string ToString()
+        {
+            return $"{Name},{Distillery},{Size},{Proof},{Age},{MSRP},{SecondaryPrice},{BottlesOwned}";
+        }
+
+
+    }
+}
---
+++ BourbonCollection/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using CsvHelper;
+using System.Globalization;
+
+namespace BourbonCollection
+{
+    class Program
+    {
+        private static List<Bottle> _bourbonBottles;
+        static void Main(string[] args)
+        {
+            // Get file path
+            string currentDirectory = Directory.GetCurrentDirectory();
+            DirectoryInfo directory = new DirectoryInfo(currentDirectory);
+            var fileName = Path.Combine(directory.FullName, "BourbonCollection.csv");
+            _bourbonBottles = ReadBourbonCollection(fileName);
+
+
+            PrintBottles(_bourbonBottles);
+        }
+
+
+        // Read data from file
+        public static List<Bottle> ReadBourbonCollection(string filepath)
+        {
+            var bourbonCollection = new List<Bottle>();
+
+            using (var reader = new StreamReader(filepath))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                bourbonCollection = csv.GetRecords<Bottle>().OrderBy(x => x.Distillery).ToList();
+            }
+
+            return bourbonCollection;
+        }
+
+        //Print bottles to console 
+        static void PrintBottles(List<Bottle> bottles)
+        {
+            foreach (var bottle in bottles)
+            {
+                Console.WriteLine(bottle);
+            }
+
+        }
+    }
+}
+
